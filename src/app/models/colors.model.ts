3aa552114e53bd060cb5843f23f45a2dcef2d56d
Parihuancollo <parihuancollodiego@gmail.com>
2025-11-11 18:57:32 -0300

    Componente de inicio creado

---
+++ src/app/models/colors.model.ts
@@ -0,0 +1,34 @@
+// export interface ILista {
+//     status: number;
+//     statusText: string;
+//     message: string;
+//     count: number;
+//     colors: Color[];
+// }
+
+export interface IColor {
+    name: string;
+    theme: Theme;
+    group: Group;
+    hex: string;
+    rgb: string;
+}
+
+export enum Group {
+    Aqua = "Aqua",
+    Blue = "Blue",
+    Brown = "Brown",
+    Cyan = "Cyan",
+    Gray = "Gray",
+    Green = "Green",
+    Orange = "Orange",
+    Pink = "Pink",
+    Purple = "Purple",
+    Red = "Red",
+    Yellow = "Yellow",
+}
+
+export enum Theme {
+    Dark = "dark",
+    Light = "light",
+}
