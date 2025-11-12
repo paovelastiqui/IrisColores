81f5599ada9e3fbc8f662b66915d4c5dfd633c66
tudiscosoftware <tudiscosoftware@gmail.com>
2025-11-12 14:48:11 +0000

    formulario

---
+++ src/app/components/contacto/contacto.component.ts
@@ -0,0 +1,45 @@
+
+import { NgClass } from '@angular/common';
+import {Component} from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contacto',
+  standalone: true,
+  imports: [ReactiveFormsModule, NgClass],
+  templateUrl: './contacto.component.html',
+  styleUrls: ['./contacto.component.css']
+})
+
+export class ContactoComponent {
+  miFormulario: FormGroup;
+
+  constructor(private _fb: FormBuilder) {
+
+    this.miFormulario = _fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: ['', [Validators.required, Validators.minLength(3)]],
+      edad: ['', [Validators.required, Validators.min(18)]],
+      tipoDni: ['', Validators.required],
+      dni: ['', [Validators.required, Validators.minLength(7)]]
+    })
+  }
+
+  get labelDocumento() {
+    const tipo = this.miFormulario.get('tipoDni')?.value;
+    return tipo? `Número de ${tipo}` : ''
+  }
+
+  enviar() {
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value)
+  }
+
+  mostrarErrores(control: string, validator: string) {
+    const campo = this.miFormulario.get(control);
+    return campo?.hasError(validator) && campo?.touched;
+  }
+}
--- src/app/components/landingPage/landingPagecomponent.ts
+++ src/app/components/landingPage/landingPagecomponent.ts
@@ -5,11 +5,12 @@ import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../hero/hero.component';
 import { PaletaComponent } from '../paleta/paleta.component';
 import { ConvertidorComponent } from '../convertidor/convertidor.component';
+import { ContactoComponent } from '../contacto/contacto.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, HeroComponent, PaletaComponent, ConvertidorComponent],
+  imports: [CommonModule, HeroComponent, PaletaComponent, ConvertidorComponent, ContactoComponent],
   templateUrl: './landingPage.component.html',
   styleUrls: ['./landingPage.component.css']
 })
